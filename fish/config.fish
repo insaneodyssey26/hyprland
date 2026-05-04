ea9710ac21d3622e424ea0f3f1839c701e0646a7
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-05-04 11:41:53 +0530

    More aliases in fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,29 @@
 set -g fish_greeting ""
 
-# 1. Startup Visuals
 if status is-interactive
-    # Runs your specific small config on startup
     fastfetch -c small.jsonc
 end
 
-# 2. Modern Aliases & Abbreviations
+# Aliases
 abbr -a ls 'eza --icons --group-directories-first'
 abbr -a ll 'eza -la --icons --group-directories-first --git'
 abbr -a tree 'eza --tree --icons'
 abbr -a cat 'bat'
-# Updated: afetch now points specifically to your small config
 abbr -a afetch 'fastfetch -c small.jsonc'
 abbr -a grep 'grep --color=auto'
+abbr -a i 'sudo pacman -S'     
+abbr -a pi 'paru -S'         
+abbr -a remove 'paru -Rns'      
+abbr -a orphans 'paru -Rns (pacman -Qtdq)' 
+abbr -a ni 'npm install'         
+abbr -a ns 'npm start'          
+abbr -a nd 'npm run dev'     
+abbr -a ff 'fastfetch'         
+abbr -a af 'fastfetch -c small.jsonc'
 
-# 3. Zoxide & Starship Initialization
+# Initials
 zoxide init fish | source
 starship init fish | source
-
-# 4. FZF Initialization
 fzf --fish | source
 
 # 5. Global Variable for 'cd' -> 'z' behavior
@@ -27,4 +31,4 @@ alias cd='z'
 
 # Keybindings
 bind \t accept-autosuggestion
-bind \b backward-kill-word # Fixed: standard key for Ctrl+H/Backspace in Fish
+bind \b backward-kill-word 
