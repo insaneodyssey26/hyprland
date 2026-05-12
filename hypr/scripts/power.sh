15af19508840f8fd771aaa2953a2dd20bcc4ff08
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-05-13 01:36:59 +0530

    Fixed fuzzel

--- hypr/scripts/power.sh
+++ hypr/scripts/power.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-options="󰐥 Power Off\n󰜉 Reboot\n󰤄 Suspend\n󰍃 Log Out"
+options="󰐥    Power Off\n󰜉    Reboot\n󰤄    Suspend\n󰍃    Log Out"
 
 choice=$(echo -e "$options" | fuzzel --dmenu -p "System  " --lines=4 --font="Geist:weight=bold:size=10")
 
 case "$choice" in
-    "󰐥 Power Off") systemctl poweroff ;;
-    "󰜉 Reboot") systemctl reboot ;;
-    "󰤄 Suspend") systemctl suspend ;;
-    "󰍃 Log Out") hyprctl dispatch exit ;;
+    *Off*) systemctl poweroff ;;
+    *Reboot*) systemctl reboot ;;
+    *Suspend*) systemctl suspend ;;
+    *Out*) hyprctl dispatch exit ;;
 esac
