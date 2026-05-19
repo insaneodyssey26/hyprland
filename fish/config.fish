b06fce022804a368f236597d2c169d0eab2448ea
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-05-20 01:38:25 +0530

    Update: add system maintenance abbreviations to config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -14,7 +14,12 @@ abbr -a grep 'grep --color=auto'
 abbr -a i 'sudo pacman -S'     
 abbr -a pi 'paru -S'         
 abbr -a rmv 'paru -Rns'      
-abbr -a orphans 'paru -Rns (pacman -Qtdq)' 
+# System Maintenance Cleanups
+abbr -a clean-orphans 'pacman -Qtdq | xargs -r sudo pacman -Rns'
+abbr -a clean-cache 'sudo pacman -Scc; and paru -Scc'
+abbr -a clean-logs 'sudo journalctl --vacuum-time=7d'
+abbr -a clean-user-cache 'rm -rf ~/.cache/*'
+abbr -a sysclean 'pacman -Qtdq | xargs -r sudo pacman -Rns; and sudo pacman -Scc; and paru -Scc; and sudo journalctl --vacuum-time=7d; and rm -rf ~/.cache/*'
 abbr -a ni 'npm install'         
 abbr -a ns 'npm start'          
 abbr -a nr 'npm run'
@@ -56,3 +61,26 @@ function done
     git commit -m "$argv"
     git push
 end
+alias theme="bash ~/.config/hypr/scripts/theme.sh"
+
+# --- MATUGEN COLOR SYNC ---
+source ~/.config/fish/colors.fish
+
+# --- FZF PREMIUM THEME ---
+set -x FZF_DEFAULT_OPTS "--color=fg:-1,bg:-1,hl:$color_primary,fg+:-1,bg+:-1,hl+:$color_primary --color=info:$color_primary,prompt:$color_primary,pointer:$color_primary,marker:$color_primary,spinner:$color_primary,header:$color_primary --inline-info --height=40% --reverse --border=rounded"
+
+# --- SUPER-JUMP (zoxide + fzf + eza preview) ---
+function zi
+    set -l result (zoxide query -i $argv)
+    if test -n "$result"
+        cd "$result"
+    end
+end
+
+# Enhanced fzf directory search with eza preview
+# Press CTRL+Alt+F to search files with a preview!
+set -x FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git"
+set -x FZF_CTRL_P_COMMAND "$FZF_DEFAULT_COMMAND"
+set -x FZF_CTRL_P_OPTS "--preview 'bat -n --color=always {}' --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+bind \cp fzf-file-widget
