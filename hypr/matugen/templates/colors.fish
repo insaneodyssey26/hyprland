e1bf86a1b974f784c2a678ca4b944747d2d4a6f4
insaneodyssey26 <hydrapikachu29@gmail.com>
2026-06-14 12:53:15 +0530

    Removed duplicate nested folders

--- hypr/matugen/templates/colors.fish
+++
@@ -1,4 +0,0 @@
-set -g color_primary "{{colors.primary.default.hex}}"
-set -g color_surface "{{colors.surface.default.hex}}"
-set -g color_on_surface "{{colors.on_surface.default.hex}}"
-set -g color_outline "{{colors.outline.default.hex}}"
