7ddb0e3fa157ca2a49fc593f8fc109f339f30a6d
Masum Ali <hydrapikachu29@gmail.com>
2026-09-14 12:44:00 +0530

    fix(setup): use stable bemoji AUR package instead of -git VCS

--- setup.sh
+++ setup.sh
@@ -65,7 +65,7 @@ else
 fi
 
 OFFICIAL_PKGS="hyprland waybar swaync fuzzel hypridle hyprlock hyprpicker hyprsunset kitty foot zsh zsh-autosuggestions zsh-syntax-highlighting eza bat fzf zoxide yazi nautilus gnome-calculator rnote satty fastfetch starship awww xdg-desktop-portal-hyprland xdg-desktop-portal-gtk polkit-kde-agent playerctl cliphist wl-clipboard xdg-user-dirs bluez bluez-utils networkmanager pipewire pipewire-pulse pipewire-alsa wireplumber pavucontrol qt5-wayland qt6-wayland brightnessctl noto-fonts-emoji unzip zip power-profiles-daemon"
-AUR_PKGS="matugen-bin nautilus-open-any-terminal brave-origin-beta-bin bemoji-git grimblast-git otf-geist maplemono-nf-unhinted wvkbd"
+AUR_PKGS="matugen-bin nautilus-open-any-terminal brave-origin-beta-bin bemoji grimblast-git otf-geist maplemono-nf-unhinted wvkbd"
 
 # Detect NVIDIA GPU and append appropriate drivers
 if lspci | grep -iE 'vga|3d' | grep -iq nvidia; then
